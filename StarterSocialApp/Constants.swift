52b5f78ea3edf6f9942bf9b4c71a285fed16f81b
PJGrantR <PJGrantR@gmail.com>
2016-10-27 15:13:56 -0400

    Sign in UI custom classes complete

---
+++ StarterSocialApp/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  StarterSocialApp
+//
+//  Created by Peter-Jon Grant on 2016-10-27.
+//  Copyright © 2016 Peter-Jon Grant. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ StarterSocialApp/FancyButton.swift
@@ -0,0 +1,23 @@
+//
+//  FancyButton.swift
+//  StarterSocialApp
+//
+//  Created by Peter-Jon Grant on 2016-10-27.
+//  Copyright © 2016 Peter-Jon Grant. All rights reserved.
+//
+
+import UIKit
+
+class FancyButton: UIButton {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.cornerRadius = 2
+    }
+
+}
