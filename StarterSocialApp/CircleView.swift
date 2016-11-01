c2891eac3c560d7fb267f1f2b51de1c371b01f2c
PJGrantR <PJGrantR@gmail.com>
2016-10-31 20:35:44 -0400

    FeedUI

---
+++ StarterSocialApp/CircleView.swift
@@ -0,0 +1,21 @@
+//
+//  CircleView.swift
+//  StarterSocialApp
+//
+//  Created by Peter-Jon Grant on 2016-10-31.
+//  Copyright © 2016 Peter-Jon Grant. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
